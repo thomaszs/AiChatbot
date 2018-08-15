f73ca6b3d0cea75e6200cd2403ffdbf8d99810b1
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-08-15 15:55:38 -0700

    Added api.ai and script

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
+'use strict';
+
+require('dotenv').config()
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
 const express = require('express');
 const app = express();
+const apiai = require('apiai')(APIAI_TOKEN);
 
 app.use(express.static(__dirname + '/views')); //html
 app.use(express.static(__dirname + '/public')); //js, css and images
 
-const server = app.listen(8080);
+const server = app.listen(process.env.PORT || 8080, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
+// Web UI
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
+
